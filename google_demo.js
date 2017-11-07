46ece171e97ace9323966b827332c56af9f9761f
Michael Suguitan <PsychoMugs@dhcp-rhodes-2971.eduroam.cornell.edu>
2017-11-06 20:56:33 -0500

    Adding updated movement files

--- google_demo.js
+++ google_demo.js
@@ -3,24 +3,59 @@ var emotions =
 	"videoID": "who2qOhwtDc",
 	"triggers": [
 		{
-			"millis": 2000,
+			"millis": 100,
+			"emotion": "idle/circle",
+			"gesture": "idle/circle"
+		},
+		{
+			"millis": 5500,
 			"emotion": "io16/calm",
 			"gesture": "io16/calm"
 		},
 		{
 			"millis": 18000,
-			"emotion": "sad2_smooth/slow_look",
-			"gesture": "sad2_smooth/slow_look"
+			"emotion": "sad3_smooth/slow_look",
+			"gesture": "sad3_smooth/slow_look"
 		},
 		{
-			"millis": 32000,
+			"millis": 33500,
 			"emotion": "fear3/fear",
 			"gesture": "fear3/fear"
 		},
 		{
-			"millis": 45000,
-			"emotion": "sad3_smooth/circle",
-			"gesture": "sad3_smooth/circle"
+			"millis": 49000,
+			"emotion": "sad2_smooth/crying",
+			"gesture": "sad2_smooth/crying"
+		},
+		{
+			"millis": 57200,
+			"emotion": "reset",
+			"gesture": "reset"
+		},
+		{
+			"millis": 59000,
+			"emotion": "happy3",
+			"gesture": "happy3"
+		},
+		{
+			"millis": 61000,
+			"emotion": "happy3_smooth",
+			"gesture": "happy3_smooth"
+		},
+		{
+			"millis": 63200,
+			"emotion": "happy2_smooth",
+			"gesture": "happy2_smooth"
+		},
+		{
+			"millis": 65500,
+			"emotion": "happy2_smooth/calm",
+			"gesture": "happy2_smooth/calm"
+		},
+		{
+			"millis": 67000,
+			"emotion": "happy2_smooth/calm",
+			"gesture": "happy2_smooth/calm"
 		},
 	]
 }
